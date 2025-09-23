321be729086def6028e5ca3517f2aa2508796182
Manish Devgan <manish.nsit8@gmail.com>
2025-09-23 21:26:12 +0200

    the class is now shared

--- model/event.py
+++ model/event.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
-# Ideally this should be shared between apps
-
 class Event(BaseModel):
     id: Optional[int] = None
     event_type: str
